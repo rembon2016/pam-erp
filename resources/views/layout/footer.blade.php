4492d52eef626bbe4009806c44f54ada6fb0845e
wirandra.rembon <alayarahman07@gmail.com>
2024-11-11 12:25:53 +0700

    update: copyright year format

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <div id="kt_app_footer" class="app-footer">
     <div class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
         <div class="text-dark order-2 order-md-1">
-            <span class="text-muted fw-semibold me-1">2023©</span>
+            <span class="text-muted fw-semibold me-1">{{ now()->format('Y') }}©</span>
             <a href="https://www.kominfo.go.id/" target="_blank" class="text-gray-800 text-hover-primary">Logi-Chain</a>
         </div>
     </div>

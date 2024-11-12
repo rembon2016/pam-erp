f1489e2e6e6e7740e66f22bab52cf87a2d289ca7
Ferdinalaxewall. <86649049+ferdinalaxewall@users.noreply.github.com>
2024-11-11 20:54:50 -0800

    Fixed Asset #7: Create Base Relation Model for Fixed Asset (Fixed Asset Type and Status)

---
+++ app/Models/Finance/FixedAssetType.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Finance;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+final class FixedAssetType extends Model
+{
+    use HasFactory,
+        HasUuids;
+
+    protected $table = 'finance.fixed_asset_types';
+    protected $guarded = ['id'];
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+}
---
+++ database/seeders/Finance/FixedAsset/FixedAssetTypeSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders\Finance\FixedAsset;
+
+use Illuminate\Database\Seeder;
+use App\Models\Finance\FixedAssetType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+final class FixedAssetTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $types = [
+            [
+                'type_name' => 'Computer Equipment',
+                'type_code' => 'CE',
+                'created_at' => now()
+            ],
+            [
+                'type_name' => 'Furniture and Fixtures',
+                'type_code' => 'FF',
+                'created_at' => now()
+            ],
+            [
+                'type_name' => 'Office Equipment',
+                'type_code' => 'OE',
+                'created_at' => now()
+            ],
+            [
+                'type_name' => 'Leasehold Improvement',
+                'type_code' => 'LI',
+                'created_at' => now()
+            ],
+            [
+                'type_name' => 'Intangible Asset',
+                'type_code' => 'IA',
+                'created_at' => now()
+            ],
+            [
+                'type_name' => 'Vehicle',
+                'type_code' => 'V',
+                'created_at' => now()
+            ],
+            [
+                'type_name' => 'Building',
+                'type_code' => 'B',
+                'created_at' => now()
+            ],
+            [
+                'type_name' => 'Land',
+                'type_code' => 'L',
+                'created_at' => now()
+            ],
+        ];
+
+        foreach ($types as $type) {
+            FixedAssetType::create($type);
+        }
+    }
+}

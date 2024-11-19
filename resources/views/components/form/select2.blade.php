a74a11f0e202a8bf8e4ca4796740440aa34287da
Ferdinalaxewall. <86649049+ferdinalaxewall@users.noreply.github.com>
2024-11-18 21:01:30 -0800

    COA #9: Implement Select2 Component in COA Form

---
+++ resources/views/components/form/select2.blade.php
@@ -0,0 +1,27 @@
+@props([
+    'label' => $label,
+    'name' => $name,
+    'placeholder' => $placeholder ?? "Select Option",
+    'required' => $required ?? false,
+    'model' => $model ?? null,
+    'multiple' => $multiple ?? false
+])
+
+<div class='mb-10'>
+    <label for="#{{ $name }}" class='form-label {{ $required ? 'required' : '' }}'>{{ $label }}</label>
+    <select class="@if($errors->has($name)) is-invalid @endif form-select"
+        name="{{ $name }}"
+        {{ $required ? 'required' : '' }}
+        id="{{ $name }}"
+        data-control="select2"
+        data-placeholder="{{ $placeholder }}"
+        @if($multiple)  data-close-on-select="false" data-allow-clear="true" multiple="multiple" @endif>
+
+        <option></option>
+        {{ $slot }}
+
+    </select>
+    <div class="invalid-feedback">
+        {{ $errors->first($name) }}
+    </div>
+</div>

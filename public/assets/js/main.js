4eb5e7ad7326c7a8651ffe46879e21353f934e33
Ferdinalaxewall. <86649049+ferdinalaxewall@users.noreply.github.com>
2024-12-06 11:24:00 +0700

    Utilities: Handle Dynamic Input Element using JavaScript

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -9,3 +9,29 @@ function generateRandomString(length) {
     }
     return result;
 }
+
+function clearValueInDynamicRow(rowItem) {
+    $(rowItem).find('input, select').val('');
+}
+
+$(document).off('click', 'button[data-type="add-dynamic-row"]').on('click', 'button[data-type="add-dynamic-row"]', function (event) {
+    const dynamicRowWrapper = $(".dynamic-row-wrapper");
+    const clonedFirstRowItem = $(dynamicRowWrapper).find('.dynamic-row-item').eq(0).clone();
+    clearValueInDynamicRow(clonedFirstRowItem);
+
+    $(dynamicRowWrapper).append(clonedFirstRowItem);
+});
+
+$(document).off('click', 'button[data-type="delete-dynamic-row"]').on('click', 'button[data-type="delete-dynamic-row"]', function (event) {
+    const dynamicRowWrapper = $(".dynamic-row-wrapper");
+    const rowItem = $(this).parents('.dynamic-row-item');
+    const remainingRowItem = $(dynamicRowWrapper).find('.dynamic-row-item').length;
+
+    if (remainingRowItem == 1) {
+        clearValueInDynamicRow(
+            $(".dynamic-row-wrapper").find('.dynamic-row-item').eq(0)
+        )
+    } else {
+        $(rowItem).remove();
+    }
+});

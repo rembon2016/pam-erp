b01fa2c035bf99fb77a219f9a78485ee03f7db99
Ferdinalaxewall. <86649049+ferdinalaxewall@users.noreply.github.com>
2024-12-09 14:46:27 +0700

    Daybook #19: Export Daybook Data to Excel, CSV, and PDF

--- app/Exports/MasterData/DaybookExport.php
+++ app/Exports/MasterData/DaybookExport.php
@@ -2,15 +2,45 @@
 
 namespace App\Exports\MasterData;
 
+use App\Service\Finance\MasterData\DaybookService;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class DaybookExport implements FromCollection
+class DaybookExport implements FromCollection, WithHeadings, WithMapping
 {
+    protected $daybookService;
+
+    public function __construct()
+    {
+        $this->daybookService = new DaybookService();
+    }
+
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        //
+        return $this->daybookService->getDaybooks();
+    }
+
+    public function map($item): array
+    {
+        return [
+            $item->code,
+            $item->name,
+            $item->type,
+            $item->description
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Daybook Code',
+            'Daybook Name',
+            'Type',
+            'Description',
+        ];
     }
 }

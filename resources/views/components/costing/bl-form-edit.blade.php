fc36e76a01e41fd5cd1d041df7e0fde20de84205
Ferdinalaxewall. <86649049+ferdinalaxewall@users.noreply.github.com>
2024-12-18 12:09:44 +0700

    Costing - SEA AIR #38: Styling Costing Tab

--- resources/views/components/costing/bl-form-edit.blade.php
+++ resources/views/components/costing/bl-form-edit.blade.php
@@ -1,6 +1,6 @@
 @foreach($costing->head as $k => $row)
     @if($row->costing_type == 'bl')
-    <ul class="nav nav-tabs" id="myTab" role="tablist">
+    <ul class="nav nav-tabs costing-tabs px-3" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
             <button class="nav-link active" id="bl-tab" data-bs-toggle="tab" data-bs-target="#bl-tab-{{ $k }}" type="button" role="tab">BL No: {{ $row->costing_value }}</button>
         </li>
@@ -10,7 +10,7 @@
 
 
     </ul>
-    <div class="tab-content p-5 bg-white border border-top-0">
+    <div class="tab-content p-5 bg-white costing-tab-content">
         <div class="tab-pane fade show active" id="bl-tab-{{ $k }}" role="tabpanel">
 
             <x-costing.charges-from :costing="$costing" :detail="$row" value="{{ $row->costing_value }}" :k="$k" type="bl" :vendor="$vendorLine" :charge="$charge" :currency="$currency" />
@@ -19,11 +19,14 @@
         </div>
 
         <div class="tab-pane fade" id="ctd-tab-{{ $k }}" role="tabpanel">
-
-          @foreach($row->shipping as $j => $ctd)
-            <div type="button" class="btn btn-success">CTD NO: {{ $ctd->ctd_number }}</div>
-            <x-costing.charges-from :costing="$costing" :detail="$row" value="{{ $ctd->ctd_number  }}" :k="$j" type="ctd" :vendor="$vendorLine" :charge="$charge" :currency="$currency" />
-          @endforeach
+            <div class="d-flex flex-column" style="row-gap: 15px;">
+                @foreach($row->shipping as $j => $ctd)
+                    <div class="">
+                        <div class="btn custom-btn btn-sm custom-btn-primary">CTD NO: {{ $ctd->ctd_number }}</div>
+                        <x-costing.charges-from :costing="$costing" :detail="$row" value="{{ $ctd->ctd_number  }}" :k="$j" type="ctd" :vendor="$vendorLine" :charge="$charge" :currency="$currency" />
+                    </div>
+                @endforeach
+            </div>
         </div>
 
 
--- resources/views/components/costing/port-form.blade.php
+++ resources/views/components/costing/port-form.blade.php
@@ -1,10 +1,13 @@
 <div class="row">
     <div class='col-md-4'>
         @if($costing?->port != null)
+        @if($costing?->port != null)
         <input type="hidden" name="costing_vendor_port_id" value="{{ $row->port->id ?? "" }}">
         @endif
+
         <x:form.select2 label="Port Code" id="port_id" name="port_id" defaultOption="Select Port Code">
             @foreach($port as $row)
+            <option value="{{ $row->port_id }}" @if($costing?->port != null && $costing?->port?->port_id == $row->port_id) selected @endif data-port-code="{{ $row->port_code }}" @if($row->port_code == "AEJEA") selected @endif>{{ $row->port_code }}</option>
             <option value="{{ $row->port_id }}" @if($costing?->port != null && $costing?->port?->port_id == $row->port_id) selected @endif data-port-code="{{ $row->port_code }}" @if($row->port_code == "AEJEA") selected @endif>{{ $row->port_code }}</option>
             @endforeach
         </x:form.select2>
@@ -16,7 +19,7 @@
             <select class="form-select" onChange="setVendorPortName('0')" name="vendor_port_id" id="vendor_port_id" data-control="select2" data-placeholder="Vendor Code">
                 <option></option>
                 @foreach($vendorPort as $rows)
-                <option value="{{ $rows->vendor_id }}" @if($costing?->port != null && $costing?->port->vendor_id == $rows->vendor_id) selected @endif data-vendor-name="{{ $rows->vendor_name }}">{{ $rows->vendor_code }}</option>
+                <option value="{{ $rows->vendor_id }}" @if($costing?->port != null && $costing?->port?->vendor_id == $rows->vendor_id) selected @endif data-vendor-name="{{ $rows->vendor_name }}">{{ $rows->vendor_code }}</option>
                 @endforeach
 
             </select>

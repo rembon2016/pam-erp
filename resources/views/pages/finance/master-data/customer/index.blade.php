809acdfc4412445bd7a8737402ec3063c382a40d
dayCod <alayarahman07@gmail.com>
2025-02-14 11:30:01 +0700

    feat: customer idiotic parameter handler

--- resources/views/pages/finance/master-data/customer/index.blade.php
+++ resources/views/pages/finance/master-data/customer/index.blade.php
@@ -113,7 +113,7 @@
                     if (item == 'customer_name') {
                         return {
                             results: pagingData.data.map(item => ({
-                                id: item.id,
+                                id: btoa(item.id + '|' + item.customer_name),
                                 text: item.customer_name
                             })),
                             pagination: {
